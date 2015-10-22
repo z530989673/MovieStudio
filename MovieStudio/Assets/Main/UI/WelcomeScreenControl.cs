47a5ea0f4d4d715a9d192fe25537f2a3fabfc59f
sunyanghaha <sunyangsecbase@gmail.com>
2015-10-22 00:15:45 -0700

    Add Basic UI screens

---
+++ MovieStudio/Assets/Main/UI/WelcomeScreenControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WelcomeScreenControl : MonoBehaviour {
+
+    // Use this for initialization
+	void Awake () {
+        Button button = GetComponentInChildren<Button>();
+        button.onClick.AddListener(delegate { UIManager.Instance.EnterGame(); } );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
