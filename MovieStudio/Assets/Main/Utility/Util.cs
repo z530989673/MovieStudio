d5b2657dfa14152f0b974989a742a5d453bc94cc
Yang Sun <yang.sun@crowdstar.com>
2015-11-03 22:45:08 -0800

    Add Json Serialize class and examples in GameInfoManager. Use Util....

---
+++ MovieStudio/Assets/Main/Utility/Util.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using FullSerializer;
+using System;
+using System.Collections.Generic;
+
+public static class Util  {
+
+	public static string Serialize(Type type, object value) {
+		fsData data;
+		(new fsSerializer()).TrySerialize(type, value, out data).AssertSuccessWithoutWarnings();
+		return fsJsonPrinter.CompressedJson(data);
+		// or pretty json;
+	}
+	
+	public static T Deserialize<T>(string serializedState) where T : class{
+		fsData data = fsJsonParser.Parse(serializedState);
+		Type type = typeof(T);
+		object deserialized = null;
+		(new fsSerializer()).TryDeserialize(data, type, ref deserialized).AssertSuccessWithoutWarnings();
+		
+		return deserialized as T;
+	}
+
+}
