aab21b72ae5aa5c5b84aa6b1f39a9c11161cf4b5
Huayu Yang <yhygoo@gmail.com>
2016-01-30 16:05:28 -0800

    Add CharacterData class

---
+++ MovieStudio/Assets/Main/GameInfo/GameData/CharacterData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum eCharacterType {
+	eCharacterTypeActor = 1,
+	eCharacterTypeTech = 2
+}
+
+public enum eCharacterAbility {
+	eCharacterAbilityNone = 0
+}
+
+public class CharacterData {
+	public int characterID;
+	public string name;
+	public int gender;
+	public string description;
+	public int rarity;
+	public int level;
+	public eCharacterType type;
+	public int appearance;
+	public int voice;
+	public int actingSkill;
+	public int creativeSkill;
+	public int artTechSkill;
+	public int musicTechSkill;
+	public int companyID;
+	public List<int> movieIDs;
+	public eCharacterAbility characterAbilityID;
+}
--- MovieStudio/Assets/Main/Scene/SceneCharactor.cs
+++ MovieStudio/Assets/Main/Scene/SceneCharactor.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SceneCharactor : SceneNode
 {
+	private CharacterData characterData;
     public SceneCharactor(GameObject parent) : base(parent) { }
     public Pair pos;
     private int currentState;
